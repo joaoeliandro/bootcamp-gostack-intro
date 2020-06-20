4bdf7df11c4a325b4cc60c711f77787db13337af
João Eliandro <eliandrogermano@gmail.com>
2020-06-20 03:41:52 -0300

    Added request to the backend

--- frontend-intro-react/src/App.js
+++ frontend-intro-react/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
 
@@ -7,20 +8,24 @@ import imageBackground from './assets/background.png';
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['teste 1', 'teste 2']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
 
     function handleAppProjects() {
-        setProjects([...projects, `teste ${projects.length + 1}`]);
+        // setProjects([...projects, `teste ${projects.length + 1}`]);
     }
 
     return (
         <>
             <Header title="Menu" />
 
-            <img width={300} src={imageBackground} />
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAppProjects}>Adicionar</button>
