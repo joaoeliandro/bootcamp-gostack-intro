f30fd6e09f1f5be0299bc66fd5040c70eb8c6282
João Eliandro <eliandrogermano@gmail.com>
2020-06-25 04:03:54 -0300

    Loading repositories

--- mobile-intro-rn/src/index.js
+++ mobile-intro-rn/src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            console.log(response.data);
+            setRepositories(response.data);
+        });
+    }, []);
+
     return (
         <>
             <StatusBar barStyle='light-content'
                 backgroundColor='#7159c1' translucent={true}
             />
-            <View style={style.container}>
-                <Text style={style.title}>Hello World!</Text>
-            </View>
+            <SafeAreaView style={style.container}>
+                <FlatList
+                    data={repositories}
+                    keyExtractor={repository => repository.id}
+                    renderItem={({ item: repository }) => (
+                        <Text style={style.title}>{repository.title}</Text>
+                    )}
+                />
+            </SafeAreaView>
         </>
     );
 }
@@ -23,7 +40,6 @@ const style = StyleSheet.create({
     },
     title: {
         color: '#fff',
-        fontSize: 32,
-        fontWeight: 'bold'
+        fontSize: 30,
     }
 });
